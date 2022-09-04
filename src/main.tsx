41360a58ecf7b8700118288dde00539f0eb920e4
isdh <hideorseekyes+github@gmail.com>
2022-09-04 13:28:25 +0900

    add comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+// React要素
 const element = <h1>Hello</h1>;
+// DOM取得
 const container = document.getElementById("root") as HTMLElement;
 
+// Reactをコンテナにレンダリング
 ReactDOM.createRoot(container).render(<React.StrictMode>{element}</React.StrictMode>);
