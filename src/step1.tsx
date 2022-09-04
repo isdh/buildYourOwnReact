0c18ae8481efe12360368241a2cc359d7d4ccf31
isdh <hideorseekyes+github@gmail.com>
2022-09-04 14:30:08 +0900

    build your own createElement and replace Reat.createElement to createElement

--- src/step1.tsx
+++ src/step1.tsx
@@ -1,6 +1,17 @@
-import React from "react";
-import { createElement } from "react";
+import React, { ReactNode } from "react";
 import ReactDOM from "react-dom/client";
+
+const createElement = (type: string, props: {}, ...children: ReactNode[]) => ({
+  // $$typeof, key, refはReact18では必須の様子
+  $$typeof: Symbol.for("react.element"),
+  key: null,
+  ref: null,
+  type,
+  props: {
+    ...props,
+    children,
+  },
+});
 
 // JSX
 // const element = (
