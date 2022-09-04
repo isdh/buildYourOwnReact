43ae53856696d1f9510a199006e4b8a8ff4e5b23
isdh <hideorseekyes+github@gmail.com>
2022-09-04 13:29:51 +0900

    replace create element method

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // React要素
-const element = <h1>Hello</h1>;
+const element = React.createElement("h1", null, "Hello");
 // DOM取得
 const container = document.getElementById("root") as HTMLElement;
 
