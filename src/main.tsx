78a239039e5562c63bc257b1a5853e5431e31e2e
isdh <hideorseekyes+github@gmail.com>
2022-09-04 13:53:29 +0900

    replace crateElement method to element object

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // React要素
-const element = React.createElement("h1", null, "Hello");
+// const reactElement = React.createElement("h1", null, "Hello");
+// createElementはオブジェクトを生成するだけなのでオブジェクトをそのまま渡しても平気
+const element = {
+  // $$typeofはReactDOMライブラリ内で付与されるシンボル
+  $$typeof: Symbol.for("react.element"),
+  key: null,
+  props: {
+    children: "Hello",
+  },
+  // refは必須になった模様
+  ref: null,
+  type: "h1",
+  // ここから下は不要
+  // _owner: null,
+  // _store: {
+  //   validated: true,
+  // },
+} as ReactElement;
+// console.log(reactElement, element);
+
 // DOM取得
 const container = document.getElementById("root") as HTMLElement;
 
