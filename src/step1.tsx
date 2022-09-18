7217404d4f0a9f05a1a4ad67a4773aaccf75fe96
isdh <hideorseekyes+github@gmail.com>
2022-09-18 22:41:12 +0900

    解消できた

--- src/step1.tsx
+++ src/step1.tsx
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom/client";
 import { Didact } from "./Didact";
 
 // JSX
+/**
+ * @link https://github.com/emotion-js/emotion/issues/2041#issuecomment-719819128
+ * 上記のコメントで動く模様
+ * viteの中にもwebpack入っているんだっけ・・・？
+ */
+/** @jsxRuntime classic */
 /** @jsx Didact.createElement */
 const element = (
   <div id="foo">
@@ -9,7 +15,10 @@ const element = (
     <b />
   </div>
 );
+
 // JSXをJSに変換する
+// /** @jsxRuntime classic */
+// /** @jsx Didact.createElement */
 // const element = Didact.createElement(
 //   "div",
 //   { id: "foo" },
